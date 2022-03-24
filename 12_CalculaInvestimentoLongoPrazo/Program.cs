997bdd7f584cbf804195c920a763fdf39c31e7e2
Vanessa Soares de Morais <vsmdantas@outlook.com>
2022-03-23 23:08:07 -0300

    Adicionar arquivos de projeto.

---
+++ 12_CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _CalculaInvestimento_LongoPrazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 12 - Calcula Investimento Longo Prazo");
+
+            double valorInvestido = 1000;
+            double fatorRendimento = 1.0036;
+
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatorRendimento;
+                }
+                fatorRendimento += 0.0010;
+                
+            }
+            Console.WriteLine("Ao termino do investimento, voce tera R$ " + valorInvestido);
+
+            Console.ReadLine();
+        }
+    }
+}
